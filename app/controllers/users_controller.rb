fcd045072b49eb252d8ce279309eda6bb43df81a
= <=>
2011-11-22 10:48:00 -0600

    after class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+   #the first thing we need to do
+   
+   #create a user
+   @user = User.new(params[:user]) # create REST resource, it extends active record. 
+   if @user.save
+     redirect_to user_path(@user)
+   else
+     @title = "Sign up"
+     render 'new'
+   end
+  end
+  
+  
+  def show
+    @user = User.find(params[:id])#get variable from the database
+    @title = @user.name
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,4 +15,29 @@ require 'spec_helper'
     end
   end
 
+
+
+
+ describe "POST 'create'" do
+    describe "success" do
+      
+      before(:each) do
+        @attr = {:name => "John Doe", :email => "jdoe@example.com"}
+      end
+      
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+    end
+  end
+  
+  
 end
